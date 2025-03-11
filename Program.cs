2a1f217b43c83e47fb97e8d4c168d10c245dc95c
Gil Shalit <gil.shalit@gmail.com>
2025-03-11 11:53:48 +0200

    with state

--- Program.cs
+++ Program.cs
@@ -10,5 +10,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7279/") });
+builder.Services.AddSingleton(sp =>
+{
+    var config = sp.GetRequiredService<IConfiguration>();
+    return new clsState(config["ODataUrl"] + "odata");
+});
+
 builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
---
+++ clsState.cs
@@ -0,0 +1,52 @@
+﻿//using LURCollectionShared;
+//using LURCollectionShared.Models;
+
+namespace SyncfusionTest
+{
+    public class clsState
+    {
+        public User currentUser { get; set; } = new User() { Role = enmRoles.Guest };
+        public string ODataUrl { get; set; }
+        //public List<PrimarySource> sources { get; set; } = new List<PrimarySource>();
+        //public Dictionary<int, string> topLevelTagNames { get; set; } = new Dictionary<int, string>();
+        //public List<Tag> lowLevelTags { get; set; }
+        //public List<Topic> Topics { get; set; } = new List<Topic>();
+        //public List<Author> Authors { get; set; } = new List<Author>();
+        //public List<OriginalSource> OriginalSources { get; set; } = new List<OriginalSource>();
+        //public List<ShowColumn> ShowColumns { get; set; } = new List<ShowColumn>();
+
+        public clsState(string oDataUrl)
+        {
+            ODataUrl = oDataUrl;
+            Console.WriteLine("state");
+        }
+        public void LogOut()
+        {
+            currentUser = new User() { Role = enmRoles.Guest };
+        }
+        #region deprecated
+        //public List<PrimarySource> hebSources { get; set; }
+        //public List<PrimarySource> latinSources { get; set; }
+        //public List<PrimarySource> helenisticSources { get; set; }
+        public Dictionary<string, string> refs { get; set; } = new Dictionary<string, string>();
+
+        #endregion deprecated
+    }
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+        public enmRoles Role { get; set; }
+    }
+    public enum enmRoles
+    {
+        Guest,
+        User,
+        SuperUser,
+        Admin
+    }
+
+}
